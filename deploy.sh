ac25679bf0261a8d2d50ae9b3d8f112451f89335
Jonathan Poisson <poisson.jonathan777@gmail.com>
2017-02-12 11:31:17 -0500

    More of the same...

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
 
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/travis_rsa
+    # ssh-add ~/.ssh/travis_rsa
     git remote add deploy "travis@beer.list.api:/opt/beer-list-api"
     git config user.name "Travis CI"
     git config user.email "jonathan.poisson777+travis@gmail.com"
