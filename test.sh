8201074d83df763be31d2f15e46b8c88e100504a
Jonathan Poisson <poisson.jonathan777@gmail.com>
2017-02-16 15:21:17 -0500

    Added exit codes to test script

--- test.sh
+++ test.sh
@@ -3,4 +3,9 @@
 docker-compose -f docker-compose.test.yml build
 docker-compose -f docker-compose.test.yml up -d
 docker logs -f beerlistapi_web_1
-docker wait beerlistapi_web_1
+EXIT_CODE="$(docker wait beerlistapi_web_1)"
+if [ "${EXIT_CODE}" = "0" ]; then
+    exit 0
+else
+    exit 1
+fi
